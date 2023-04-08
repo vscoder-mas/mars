94dd0612ccc9d2d39feadd9884adccffec9b89ac
mashuai <cn_global@hotmail.com>
2023-04-08 19:46:11 +0800

    chore: CMakeLists.txt 注释

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "f8f9a83b"
+#define MARS_REVISION "5a33939f"
 #define MARS_PATH "ms/build"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2023-04-08 18:44:33"
+#define MARS_BUILD_TIME "2023-04-08 19:34:52"
 #define MARS_TAG "1"
 
 #endif
